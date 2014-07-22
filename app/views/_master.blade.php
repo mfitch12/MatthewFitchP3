8f2f5a4aeea31751f2f266c71269bdf710a34674
Matt Fitch <mrf088@gmail.com>
2014-07-22 15:02:18 -0400

    added JQuery to make console happy

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -37,9 +37,13 @@
 
     </div> <!-- /container -->
 
+    <!-- JQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
     <!-- JavaScript -->
     <script src='js/bootstrap.min.js'></script>
+
+
 
   </body>
 </html>
